8588bf23d5c3338de94eda97e35bc945b9bc1a64
Rajguru09 <rajesh09guru@gmail.com>
2025-05-13 17:39:30 +0530

    Update api_routes.py

--- backend/app/api_routes.py
+++ backend/app/api_routes.py
@@ -1,1 +1,8 @@
 
+from fastapi import APIRouter
+from app.api import auth
+
+api_router = APIRouter()
+
+# Add all routers here
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
