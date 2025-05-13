b67a25d80125117eb8165eff260a6a170da4818a
Rajguru09 <rajesh09guru@gmail.com>
2025-05-13 16:45:20 +0530

    Update App.jsx

--- frontend/src/assets/components/App.jsx
+++ frontend/src/assets/components/App.jsx
@@ -1,1 +1,19 @@
 
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import AWSCredentials from './pages/AWSCredentials';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/aws-credentials" element={<AWSCredentials />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
